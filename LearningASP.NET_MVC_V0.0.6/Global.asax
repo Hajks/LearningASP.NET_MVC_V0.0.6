0bb2081d667dfb8e20c7fe6bf5044fab2151f64e
Hajk <tunczorx@gmail.com>
2018-10-17 13:03:25 +0200

    Add project files.

---
+++ LearningASP.NET_MVC_V0.0.6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearningASP.NET_MVC_V0._0._6.MvcApplication" Language="C#" %>
---
+++ LearningASP.NET_MVC_V0.0.6/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LearningASP.NET_MVC_V0._0._6
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
