736819697156a0bf1064439a1355cd4eca1942f0
Hajk <tunczorx@gmail.com>
2018-10-24 13:22:31 +0200

    Learning AJAX

---
+++ LearningASP.NET_MVC_V0.0.6/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningASP.NET_MVC_V0._0._6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetTime()
+        {
+            Thread.Sleep(2000);
+            return DateTime.Now.ToLongTimeString();
+        }
+    }
+}
